c37215f5f4917915f8024f4a5218d4071f8c93d1
Byggvir <thomas@arend-rhb.de>
2016-01-05 17:12:17 +0100

    Inserted a new start page and included old default status page.

--- files/lib/gluon/config-mode/wizard/0510-website-info.lua
+++ files/lib/gluon/config-mode/wizard/0510-website-info.lua
@@ -6,26 +6,40 @@ local M = {}
 
 function M.section(form)
   local s = form:section(cbi.SimpleSection, nil, i18n.translate(
-    'You can provide your homepage address here to '
+    'You can provide your name and homepage here to '
       .. 'allow a link on the status page. Please note that '
       .. 'this information will be visible <em>publicly</em> '
       .. 'on the internet together with your node\'s coordinates.'
     )
   )
 
-  local o = s:option(cbi.Value, "_homepage", i18n.translate("Homepage info"))
+  local o
+  
+  o = s:option(cbi.Value, "_name", i18n.translate("Owner's name"))
+  o.default = uci:get_first("gluon-node-info", "owner", "name", "")
+  o.rmempty = true
+  o.datatype = "string"
+  o.description = i18n.translate("Owner name (e.g. Jane Doe)")
+  o.maxlen = 40
+
+  o = s:option(cbi.Value, "_homepage", i18n.translate("Owner's homepage"))
   o.default = uci:get_first("gluon-node-info", "owner", "homepage", "")
   o.rmempty = true
   o.datatype = "string"
-  o.description = i18n.translate("Address of your homepage (http://example.com/freifunk)")
+  o.description = i18n.translate("Owner's homepage (e.g. http://example.com)")
   o.maxlen = 140
+  
 end
 
 function M.handle(data)
-  if data._contact ~= nil then
-    uci:set("gluon-node-info", uci:get_first("gluon-node-info", "owner"), "homepage", data._website)
+  local sname = uci:get_first("gluon-node-info", "owner")
+  
+  if data._name ~= nil and data._homepage~= nil then
+    uci:set("gluon-node-info", sname, "name", data._name)
+    uci:set("gluon-node-info", sname, "homepage", data._homepage)
   else
-    uci:delete("gluon-node-info", uci:get_first("gluon-node-info", "owner"), "homepage")
+    uci:delete("gluon-node-info", sname, "name")
+    uci:delete("gluon-node-info", sname, "homepage")
   end
   uci:save("gluon-node-info")
   uci:commit("gluon-node-info")
